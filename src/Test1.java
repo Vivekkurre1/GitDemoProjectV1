3f95450da053ea4897e7f46aaab8aabf5c602e3e
Vivekkurre1 <vivekkurre1@outlook.com>
2023-01-17 22:05:32 +0530

    Commit one ......

---
+++ src/Test1.java
@@ -0,0 +1,12 @@
+
+public class Test1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry="yes";
+		if(hungry == "yesh") {
+			System.err.println("Do it");
+		}else {
+			System.out.println("Do Work");
+		}
+	}
+}
